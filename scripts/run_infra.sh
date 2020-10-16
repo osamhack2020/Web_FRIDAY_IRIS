55fa777956ef6bef92bf2bfeb6e24083e9e353ae
Deokho Jeong <duckhoim@kyonggi.ac.kr>
2020-10-16 14:38:32 +0000

    script(run infra)!: add run infra script That script use OSAM2020/Infra_FRIDAY_IRIS

---
+++ scripts/run_infra.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# check already running
+if [ "$(docker ps -q -f name=haproxy)" ]; then
+    read -p " If you wanna re compose infra ??? [y/n]" req_rebuild
+    if [ ${req_rebuild} == 'y' ] || [ ${req_rebuild} == 'Y' ]; then
+        docker-compose down -v
+    else
+        echo "Exit this script"
+        exit 0
+    fi
+fi
+download='y'
+# Ask will be use local source ( or downloaded already ) 
+if [ -d "Infra_FRIDAY_IRIS" ]; then
+    read -p " Would you use local source ? ( or downloaded already ) [y/n]" req_redown
+    if [ ${req_redown} == 'n' ] || [ ${req_redown} == 'N' ]; then
+        rm -rf Infra_FRIDAY_IRIS
+        echo 'We will re download'
+    else
+        echo "Will be compose using local source"
+        download='n'
+    fi
+fi
+if [ ${download} == 'y' ]; then
+    read -p " If you are developing in Codespace ? [y/n] " in_codespace
+    if [ ${in_codespace} == 'y' ] || [ ${in_codespace} == 'Y' ]; then
+        git clone -b codespace --single-branch https://github.com/osamhack2020/Infra_FRIDAY_IRIS.git
+    elif [ ${in_codespace} == 'n' ] || [ ${in_codespace} == 'N' ]; then
+        git clone https://github.com/osamhack2020/Infra_FRIDAY_IRIS.git
+    fi
+fi
+if ! command -v docker-compose &> /dev/null
+then
+    echo "Install docker-script"
+    chmod +x install_docker-compose.sh
+    ./install_docker-compose.sh
+fi
+cd Infra_FRIDAY_IRIS/database
+# Delete Write permission at config file
+chmod a=rx master/*.cnf slave/*.cnf
+docker-compose up -d --build
+cd ../..
+rm -rf Infra_FRIDAY_IRIS
