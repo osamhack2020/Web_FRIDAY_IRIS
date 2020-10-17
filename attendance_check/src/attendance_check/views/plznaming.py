46f98a5210acc9388f206e0d519a4d7a11f7fea2
l0vey0u <duckhoim@kyonggi.ac.kr>
2020-10-17 15:28:44 +0900

    feat(dbapi) : 데이터가 미 존재 시 예외 처리 추가

--- attendance_check/src/attendance_check/views/plznaming.py
+++ attendance_check/src/attendance_check/views/plznaming.py
@@ -7,7 +7,11 @@ plznaming = Blueprint('plznaming', __name__)
 @plznaming.route('/group/<supply_name>', methods=['GET'])
 def get_group_list(supply_name):
     row = SupplyList.query.filter_by(name=supply_name).first()
+    if not row:
+        return "등록되지 않은 배식장소 입니다."
     group_list = GroupList.query.filter_by(supply_id=row.id)
+    if not group_list:
+        return "해당 배식장소를 이용하는 그룹이 없습니다."
     return jsonify(
         group_list=[row.name for row in group_list]
     )
