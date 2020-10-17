d7b7d9fa63736d40dc8f32080e3dc605c3152bbe
l0vey0u <duckhoim@kyonggi.ac.kr>
2020-10-17 17:29:28 +0900

    style(dbapi) : HTTP상태코드 포함 응답, 에러 코드는 숫자로 타입변환

--- attendance_check/src/attendance_check/views/groups.py
+++ attendance_check/src/attendance_check/views/groups.py
@@ -9,16 +9,16 @@ def get_group_list(supply_name):
     row = SupplyList.query.filter_by(name=supply_name).first()
     if not row:
         return jsonify(
-            Error_Code = '404_1',
-            msg = "등록되지 않은 배식장소 입니다."
-        )
+            error_id = 1,
+            msg = "등록되지 않은 배식소 입니다."
+        ), 404
     group_list = GroupList.query.filter_by(supply_id=row.id)
     if not group_list:
         return jsonify(
-            Error_Code = '404_2',
-            msg = "해당 배식장소를 이용하는 그룹이 없습니다."
-        )
+            error_id = 2,
+            msg = "해당 배식소를 이용하는 그룹이 없습니다."
+        ), 404
         
     return jsonify(
         group_list=[row.name for row in group_list]
-    )
+    ), 200
--- attendance_check/src/attendance_check/views/members.py
+++ attendance_check/src/attendance_check/views/members.py
@@ -9,17 +9,17 @@ def get_member_info(member_id):
     row = GroupMemberInfo.query.filter_by(member_id=member_id).first()
     if not row:
         return jsonify(
-            Error_Code = '404_3',
+            error_id = 1,
             msg = "멤버 정보를 조회할 수 없습니다."
-        )
+        ), 404
     row2 = GroupList.query.filter_by(id=row.group_id).first()
     if not row2:
         return jsonify(
-            Error_Code = '404_4',
+            error_id = 2,
             msg = "그룹 정보가 등록되어 있지 않은 사용자입니다."
-        )
+        ), 404
     return jsonify(
         ID = row.member_id,
         NAME = row.name,
         Group = row2.name
-    )
+    ), 200
